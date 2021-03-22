5999fd8817bed31a9868709485c6ad2b6c90dd7d
Aaron Lui <aaron_lui@163.com>
2021-03-22 21:35:26 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+  el: '#app',
+  data() {
+    return {
+      favList: [
+        {
+          icon: 'https://www.google.com/favicon.ico',
+          url: 'https://www.google.com',
+          title: '谷歌'
+        }
+      ]
+    }
+  },
+  created() {
+  }
+})
