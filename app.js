9ddb014150c9c07a4b44469627a40bf444b63487
Aaron Lui <aaron_lui@163.com>
2021-03-23 17:55:23 +0800

    修改样式

--- app.js
+++ app.js
@@ -2,15 +2,40 @@ var app = new Vue({
   el: '#app',
   data() {
     return {
+      query: '',
       favList: [
         {
-          icon: 'https://www.google.com/favicon.ico',
-          url: 'https://www.google.com',
-          title: '谷歌'
+          url: 'https://www.aaronlui.net',
+          title: 'Aaron\'s blog'
+        },
+        {
+          url: 'https://www.sohu.com',
+          title: '搜狐'
+        },
+        {
+          url: 'https://weibo.com',
+          title: '微博'
+        },
+        {
+          url: 'https://sspai.com',
+          title: '少数派'
+        },
+        {
+          url: 'https://www.dgtle.com',
+          title: '数字尾巴'
+        },
+        {
+          url: 'https://www.ithome.com',
+          title: 'IT之家'
         }
       ]
     }
   },
   created() {
+  },
+  methods: {
+    search() {
+      window.open(`http://www.baidu.com/s?wd=${this.query}`)
+    }
   }
 })
